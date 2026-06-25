e1e5e6978c19fcec21e7ed28dfe71a795c6f66f7
Kiran <erkiran15@gmail.com>
2026-06-25 16:18:44 +0530

    Added Ujjain module

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace Himalayan.Controllers
         {
             return View();
         }
-
+        public IActionResult Privacy2()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
